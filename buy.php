4ed9247774f0cd2a9ddf1883bf2e234bb28739b8
Bandita <36320678+BanditaHota@users.noreply.github.com>
2018-11-12 21:09:07 -0500

    Add files via upload

---
+++ buy.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title> Thank You</title>
+</head>
+
+<body>
+
+<div id="buy">
+<center><b> Thank You for shopping with Us!!</b> </center>
+</div>
+
+<div style="width:650px; margin:0 auto;">
+<img src="images/buy.jpg">
+</div>
+
+
+</body>
+</html>
